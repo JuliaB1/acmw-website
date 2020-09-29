5e227a48c439c0aedb1bb8394d968f4611bdca84
JuliaB1 <juliabaylon01@gmail.com>
2020-09-28 17:14:09 -0700

    added new W motif to landing page

---
+++ src/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Landing.css';
+
+function Landing(){
+  return (
+    <div>
+        <img src={'/img/w_motif_applied.png'} className="Background-img" alt=""/>
+    </div>
+  );
+}
+
+export default Landing;
