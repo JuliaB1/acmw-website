e9a0eabed22318f974d1310c93fb8bc3fa23d971
JuliaB1 <juliabaylon01@gmail.com>
2020-10-10 08:51:51 -0700

    adding initial text

--- src/Landing.js
+++ src/Landing.js
@@ -3,8 +3,16 @@ import './Landing.css';
 
 function Landing(){
   return (
-    <div>
-        <img src={'/img/header_gradient_text.svg'} className="Background-img" alt=""/>
+    <div className="main">
+        <img
+          src={"/img/header_gradient_text.svg"}
+          className="Header-img"
+          alt=""
+        />
+        <div className="section-div">
+      <div className="section-header">Our Mission</div>
+      <div>We advocate for the inclusion of all groups in computer science!</div>
+      </div>
     </div>
   );
 }
